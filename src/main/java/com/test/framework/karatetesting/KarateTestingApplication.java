c62a03dd2762a1fa3a8c333f611b9bc5cb3b0f91
Kavish Rizvi <rizvikavish205@gmail.com>
2021-09-13 18:59:42 +0530

    first commit

---
+++ src/main/java/com/test/framework/karatetesting/KarateTestingApplication.java
@@ -0,0 +1,13 @@
+package com.test.framework.karatetesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarateTestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KarateTestingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/test/framework/karatetesting/KarateTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.framework.karatetesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KarateTestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
